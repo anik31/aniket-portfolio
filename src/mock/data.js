66b04c7233f326b54f8a2c8a2c26f1a5468a7e6a
iamrajmani <b218004@iiit-bh.ac.in>
2021-03-22 10:46:57 +0530

    Added files of portfolio website

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,67 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Aniket - My Portfolio', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello, my name is',
+  name: 'Aniket Prakash',
+  subtitle: 'I am a web developer.',
+  cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'linked1.jpg',
+  paragraphOne:
+    "Hi 👋 I'm Aniket Prakash, A Web Developer with an interest in Backend development. I’m currently situated in Odisha, India.",
+  paragraphTwo:
+    'I love to explore technologies and look forward to opportunities to create meaningful products.',
+  paragraphThree: ' ',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'minder.png',
+    title: 'Minder',
+    info: 'Minder is an interface for movie cards where a user can Like or dislike a movie.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'http://www.minder.live',
+    repo: 'https://github.com/anik31/minder.git', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'shrinker.png',
+    title: 'Shrinker',
+    info: 'URL shortener web app made using node.js and mongodb.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://shrinker-app.herokuapp.com/',
+    repo: 'https://github.com/anik31/shrinker', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'keeper.png',
+    title: 'Keeper',
+    info: 'Notepad app made using ReactJS.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://keeper-app-one.vercel.app/',
+    repo: 'https://github.com/anik31/keeper-app', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Screenshot (670).png',
+    title: 'Simon Game',
+    info: 'Simple game to press the buttons according to the sequence.',
+    info2: '',
+    url: 'https://anik31.github.io/simon-game/',
+    repo: 'https://github.com/anik31/simon-game', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +70,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'b218004@iiit-bh.ac.in',
 };
 
 // FOOTER DATA
@@ -68,27 +79,32 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/_anik_31',
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'instagram',
+      url: 'https://www.instagram.com/aniket_prakash_31/',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/anik31/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/anik31',
+    },
+    {
+      id: nanoid(),
+      name: 'medium',
+      url: 'https://anik31.medium.com/',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
